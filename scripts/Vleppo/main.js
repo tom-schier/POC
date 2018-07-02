0eba9fe9dae8a7b9ae7003d915bcb283b6a74a6d
tom-schier <schiertxs@gmail.com>
2018-07-02 16:14:25 +1000

    Reformatted, updates, subscribe to known channel

--- scripts/Vleppo/main.js
+++ scripts/Vleppo/main.js
@@ -17,24 +17,30 @@
 //     this.vp = new Vleppo();
 //     let channel = vp.createChannel();
 // }
-
+currentChannelStr = "";
+currentUser = "";
+iota = new IOTA({
+    'host': 'https://nodes.testnet.iota.org',
+    'port': 443
+});
+channelRoot = "";
 
  class Vleppo {
     
     get mamState() { return this._mamState; }
     set mamState(value) { this._mamState = value;}
 
-    get iota() { return this._iota; }
-    set iota(value) { this._iota = value;}
+    // get iota() { return this._iota; }
+    // set iota(value) { this._iota = value;}
 
-    get channelRoot() { return this._channelRoot; }
-    set channelRoot(value) { this._channelRoot = value;}
+    // get channelRoot() { return this._channelRoot; }
+    // set channelRoot(value) { this._channelRoot = value;}
 
     get Mam() { return this._Mam; }
     set Mam(value) { this._Mam = value;}
 
-    get currentChannelStr() { return this._currentChannelStr; }
-    set currentChannelStr(value) { this._currentChannelStr = value;}
+    // get currentChannelStr() { return this._currentChannelStr; }
+    // set currentChannelStr(value) { this._currentChannelStr = value;}
 
     constructor() {
       //  this.connectToIota();
@@ -46,30 +52,30 @@
         //var tag = document.getElementById("itemTag").value;
         var itemObj = { "descr": descr};
 
-        this.connectToIota();
+        //this.connectToIota();
 
-        let trytes = this.iota.utils.toTrytes(JSON.stringify(itemObj));
-        this.mamState = this.Mam.init(this.iota, trytes, 2);
+        let trytes = iota.utils.toTrytes(JSON.stringify(itemObj));
+        this.mamState = this.Mam.init(iota, trytes, 2);
 
         // Attach the payload
         let message = this.Mam.create(this.mamState, trytes);
         this.mamState = message.state;
         let obj = await this.Mam.attach(message.payload, message.address);
         
-        this.channelRoot = message.root;
-        this.currentChannelStr = descr;
+        channelRoot = message.root;
+        currentChannelStr = descr;
         this.retrieve();
-        document.getElementById("messageRoot").value = this.channelRoot;
+        document.getElementById("messageRoot").value = channelRoot;
         return message.root;    
     }
 
-    connectToIota() {
-        let iota = new IOTA({
-            'host': 'https://nodes.testnet.iota.org',
-            'port': 443
-        });
-        this.iota = iota;
-    }
+    // connectToIota() {
+    //     let iota = new IOTA({
+    //         'host': 'https://nodes.testnet.iota.org',
+    //         'port': 443
+    //     });
+    //     //this.iota = iota;
+    // }
 
     getDateAndTime() {
         let a = new Date();
@@ -85,16 +91,16 @@
 
 
     async publish() {
-        if (this.iota == null) {
-            this.connectToIota();
-        }
+        // if (iota == null) {
+        //     this.connectToIota();
+        // }
         var price = document.getElementById("itemPrice").value;
-
         let dateStamp = this.getDateAndTime();
-
-        var itemObj = { "user": "Tomas", "price": price, "date": dateStamp};
+        this.mamState = this.Mam.init(iota, trytes, 2);
+        currentUser = "Tomas";
+        var itemObj = { "user": currentUser, "price": price, "date": dateStamp, "channel": currentChannelStr};
         // Create MAM Payload
-        let trytes = this.iota.utils.toTrytes(JSON.stringify(itemObj));
+        let trytes = iota.utils.toTrytes(JSON.stringify(itemObj));
         let message = this.Mam.create(this.mamState, trytes);
         // Save new mamState
         this.mamState = message.state;
@@ -103,8 +109,24 @@
         console.log("Publich succeded")
     }
 
-    async retrieve() {
-        let theRoot = this.channelRoot;
+    async retrieve(ch) {
+        let theRoot = "";
+        let aVal = document.getElementById("channelNew").value;
+        if(ch == "NEW" && aVal != null) {
+            
+            theRoot = aVal;
+         
+            var numRows = document.getElementById("offerTableBody").getElementsByTagName("tr").length;
+            for (let i = 0; i < numRows; i++) {
+                document.getElementById("offerTableBody").deleteRow(i);
+            }
+            channelRoot = aVal;
+
+        } else {
+            theRoot = channelRoot;            
+        }
+
+        document.getElementById("messageRoot").value = channelRoot;
         while (true) {
             let resp = await this.Mam.fetch(theRoot, null, null, this.parseReturnValues);
             if (resp != null)
@@ -113,20 +135,18 @@
     }
 
     parseReturnValues(data){
-        let iota = new IOTA({
-            'host': 'https://nodes.testnet.iota.org',
-            'port': 443
-        });
+        // let iota = new IOTA({
+        //     'host': 'https://nodes.testnet.iota.org',
+        //     'port': 443
+        // });
+
         let json = JSON.parse(iota.utils.fromTrytes(data));
         if (json != null) {
 
-            // Find a <table> element with id="myTable":
             var table = document.getElementById("offerTableBody");
 
-            // Create an empty <tr> element and add it to the 1st position of the table:
             var row = table.insertRow(0);
 
-            // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
             var cell1 = row.insertCell(-1); //USer
             cell1.className += " col text-overflow: ellipsis";
             var cell2 = row.insertCell(-1); //Item
@@ -136,14 +156,14 @@
             var cell4 = row.insertCell(-1); //Date
             cell4.className += " col text-overflow: ellipsis";
             if (json.descr != null) {
-                //this.currentChannelStr = json.descr;
+                currentChannelStr = json.descr;
                 cell1.innerHTML = "";
                 cell2.innerHTML = json.descr;
                 cell3.innerHTML = "";   
                 cell4.innerHTML = "";
             } else {
                 cell1.innerHTML = json.user;
-                cell2.innerHTML = "";
+                cell2.innerHTML = currentChannelStr;
                 cell3.innerHTML = json.price;   
                 cell4.innerHTML = json.date;
             }
